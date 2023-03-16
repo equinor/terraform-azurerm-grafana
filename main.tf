5480185c9a0a970701bf6291b0bd5d6dcab889ed
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-03-16 06:40:59 +0100

    refactor: set explicit sku

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "azurerm_dashboard_grafana" "this" {
   name                              = var.instance_name
   resource_group_name               = var.resource_group_name
   location                          = var.location
+  sku                               = "Standard"
   api_key_enabled                   = false
   deterministic_outbound_ip_enabled = false
   public_network_access_enabled     = true
