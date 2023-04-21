b686e9cef410da116edd2e31ff873e0e88e724fb
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-04-21 10:57:52 +0200

    new folder

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -3,15 +3,3 @@ variable "location" {
   type        = string
   default     = "northeurope"
 }
-
-# variable "service_principal_client_id" {
-#   type    = string
-#   default = "7a2ba5d9-d06e-466a-83d5-0284953a1f15"
-#   # sensitive = true
-# }
-
-# variable "service_principal_client_secret" {
-#   type    = string
-#   default = "641d1ede-7340-4306-9370-3aad90197a8b"
-#   # sensitive = true
-# }
--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,10 +1,1 @@
-# terraform {
-#   required_version = ">= 1.0.0"
 
-#   required_providers {
-#     grafana = {
-#       source  = "grafana/grafana"
-#       version = "1.37.0"
-#     }
-#   }
-# }
